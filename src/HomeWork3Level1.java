c32075219e2529029d9f9bc5323cebe223bac36e
par55 <par555@mail.ru>
2022-11-24 16:44:46 +0100

    HomeWork#3

---
+++ src/HomeWork3Level1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class HomeWork3Level1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int rubles = scanner.nextInt();
+        int dollars = scanner.nextInt();
+        System.out.println( rubles / dollars);
+        System.out.println( rubles * dollars );
+        System.out.println( rubles - dollars );
+        System.out.println( rubles + dollars);
+
+
+
+    }
+}
